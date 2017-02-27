e5949f269a9b7fb34af75018231d91fd0060aa97
Luka Bartoniček <luka.bartonicek@gmail.com>
2017-02-28 00:15:54 +0100

    Topic Parser

--- addons/binding/org.openhab.binding.ctrlhome/src/main/java/org/openhab/binding/ctrlhome/internal/homie/Topic.java
+++ addons/binding/org.openhab.binding.ctrlhome/src/main/java/org/openhab/binding/ctrlhome/internal/homie/Topic.java
@@ -1,6 +1,79 @@
 package org.openhab.binding.ctrlhome.internal.homie;
 
 public class Topic {
+    private String baseTopic;
+    private String deviceFwName;
+    private String deviceFwVersion;
+    private String deviceHomie;
     private String deviceId;
+    private String deviceImplementation;
+    private String deviceLocalIp;
+    private String deviceMac;
+    private String deviceName;
+    private String deviceOnline;
+    private String deviceStatsSignal;
+    private String deviceStatsUptime;
     private String nodeId;
+
+    public void setBaseTopic(String baseTopic) {
+        this.baseTopic = baseTopic;
+    }
+
+    public void setDeviceFwName(String deviceFwName) {
+        this.deviceFwName = deviceFwName;
+    }
+
+    public void setDeviceFwVersion(String deviceFwVersion) {
+        this.deviceFwVersion = deviceFwVersion;
+    }
+
+    public void setDeviceHomie(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceId(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceImplementation(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceLocalIp(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceMac(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceName(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceOnline(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceStatsSignal(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setDeviceStatsUptime(String string) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setNodeId(String string) {
+        // TODO Auto-generated method stub
+
+    }
 }
--- addons/binding/org.openhab.binding.ctrlhome/src/main/java/org/openhab/binding/ctrlhome/internal/homie/TopicParser.java
+++ addons/binding/org.openhab.binding.ctrlhome/src/main/java/org/openhab/binding/ctrlhome/internal/homie/TopicParser.java
@@ -3,6 +3,19 @@ package org.openhab.binding.ctrlhome.internal.homie;
 import java.text.ParseException;
 
 public class TopicParser {
+    private final String HOMIE_PROPERTY_PREFIX = "$";
+    private final String HOMIE_DEVICE_PROPERTY_HOMIE = "homie";
+    private final String HOMIE_DEVICE_PROPERTY_ONLINE = "online";
+    private final String HOMIE_DEVICE_PROPERTY_NAME = "name";
+    private final String HOMIE_DEVICE_PROPERTY_LOCAL_IP = "localip";
+    private final String HOMIE_DEVICE_PROPERTY_MAC = "mac";
+    private final String HOMIE_DEVICE_PROPERTY_STATS = "stats";
+    private final String HOMIE_DEVICE_PROPERTY_STATS_UPTIME = "uptime";
+    private final String HOMIE_DEVICE_PROPERTY_STATS_SIGNAL = "signal";
+    private final String HOMIE_DEVICE_PROPERTY_FW = "fw";
+    private final String HOMIE_DEVICE_PROPERTY_FW_NAME = "name";
+    private final String HOMIE_DEVICE_PROPERTY_FW_VERSION = "version";
+    private final String HOMIE_DEVICE_PROPERTY_IMPLEMENTATION = "implementation";
     private String baseTopic;
 
     public TopicParser(String baseTopic) {
@@ -15,11 +28,52 @@ public class TopicParser {
 
     public Topic parse(String topicString) throws ParseException {
         String[] elements = topicString.split("\\/");
+        Topic topic = new Topic();
+        if (elements.length > 1) {
+            topic.setDeviceId(elements[1]);
+            if (elements.length > 2) {
+                if (elements[2].startsWith(HOMIE_PROPERTY_PREFIX)) {
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_HOMIE)) {
+                        topic.setDeviceHomie(elements[3]);
+                    }
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_ONLINE)) {
+                        topic.setDeviceOnline(elements[3]);
+                    }
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_NAME)) {
+                        topic.setDeviceName(elements[3]);
+                    }
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_LOCAL_IP)) {
+                        topic.setDeviceLocalIp(elements[3]);
+                    }
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_MAC)) {
+                        topic.setDeviceMac(elements[3]);
+                    }
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_STATS)) {
+                        if (elements[3].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_STATS_UPTIME)) {
+                            topic.setDeviceStatsUptime(elements[4]);
+                        }
+                        if (elements[3].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_STATS_SIGNAL)) {
+                            topic.setDeviceStatsSignal(elements[4]);
+                        }
+                    }
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_FW)) {
+                        if (elements[3].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_FW_NAME)) {
+                            topic.setDeviceFwName(elements[4]);
+                        }
+                        if (elements[3].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_FW_VERSION)) {
+                            topic.setDeviceFwVersion(elements[4]);
+                        }
+                    }
+                    if (elements[2].equals(HOMIE_PROPERTY_PREFIX + HOMIE_DEVICE_PROPERTY_IMPLEMENTATION)) {
+                        topic.setDeviceImplementation(elements[3]);
+                    }
+                } else {
+                    topic.setNodeId(elements[2]);
+                }
+            }
+        }
 
         return new Topic();
     }
 
-    public void setBaseTopic(String baseTopic) {
-        this.baseTopic = baseTopic;
-    }
 }
